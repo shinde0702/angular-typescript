cc0c521a8da5546e289995021b8eb7153bf6447a
shinde0702 <abhijeetshinde596@gmail.com>
2023-01-15 11:29:42 +0530

    save

--- function.js
+++ function.js
@@ -1,10 +1,18 @@
 function fun(num1) {
-    console.log('hello', num1);
+    console.log('hello', +num1);
 }
 fun(200);
-
 function abhi(num2, num3) {
     return num2 * num3;
 }
-var m = abhi(10, 2);
-console.log(m);
+var n = abhi(10, 2);
+console.log(n);
+function vishal(b) {
+    console.log('hello vishal', +b);
+}
+vishal(23);
+function k(num3, num4) {
+    return num3 * num4;
+}
+var l = k(36, 5);
+console.log(l);
--- function.ts
+++ function.ts
@@ -1,6 +1,6 @@
 function fun(num1:number)
 {
-    console.log('hello',num1);
+    console.log('hello',+num1);
 }
 fun(200);
 
@@ -8,5 +8,20 @@ fun(200);
 function abhi(num2:number,num3:number):number{
     return num2*num3
 }
-let m:number=abhi(10,2)
-console.log(m)
+let n:number=abhi(10,2)
+console.log(n)
+
+
+function vishal(b:number){
+    console.log('hello vishal', +b)
+}
+vishal(23)
+
+
+
+function k(num3:number,num4:number):number{
+    return num3*num4 ;
+}
+let l:number=k(36,5)
+
+console.log(l)
